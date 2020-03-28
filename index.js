56b923298f147a72bda796a9d3fa3b7097c01078
Ronald M. Kasendwa <kasendwa@awamo.com>
2020-03-28 23:17:36 +0300

    Implements todo menu

---
+++ index.js
@@ -0,0 +1,10 @@
+const getMenu = function() {
+	return `
+Welcome to Todo CLI!
+
+--------------------
+
+(v) View • ( n ) New • (cX) Complete • (dX) Delete • (q) Quit
+	`;
+};
+console.log(getMenu());
