efee458fb3bd435f054a57786a7b742d695ed9a9
Ronald M. Kasendwa <kasendwa@awamo.com>
2020-03-29 00:20:19 +0300

    Implements todo list

--- index.js
+++ index.js
@@ -3,32 +3,59 @@ const rl = readline.createInterface({
 	input: process.stdin,
 	output: process.stdout
 });
+const todoList = [];
 rl.on("close", function() {
-	console.log("\nBYE BYE !!!");
+	console.log("\nGood bye!");
 	process.exit(0);
 });
-const prompt = function() {
-	rl.question(getMenu(), function(command) {
-		switch (command.toLowerCase()) {
-			case 'q':
-				rl.close();
-				break;
-			default:
-				prompt();
+const prompt = function(message = '') {
+	rl.question(`${message}${getMenu()}`, command => {
+		const indexMath = /^c(\d+)$/g.exec(command);
+		if (indexMath) {
+			const index = parseInt(indexMath[1]);
+			if (todoList[index]) {
+				todoList[index].done = true;
+				console.log(`Completed "${todoList[index].name}"\n`);
+			}
+			prompt();
+		} else {
+			switch (command.toLowerCase()) {
+				case 'q':
+					rl.close();
+					break;
+				case 'v':
+					prompt(todoList.map((listItem, index) => `${index} [${listItem.done ? '✓' : ' '}] ${listItem.name}`).join('\n') + '\n');
+					break;
+				case 'n':
+					rl.question(`
+What?
+
+>					`, todoName => {
+						todoList.push({
+							done: false,
+							name: todoName
+						});
+						prompt();
+					});
+					break;
+				default:
+					prompt();
+			}
 		}
 	});
 };
 const getMenu = function() {
 	return `
-Welcome to Todo CLI!
-
---------------------
-
 (v) View • ( n ) New • (cX) Complete • (dX) Delete • (q) Quit
 
 >	`;
 };
 const init = function() {
+	console.log(`
+Welcome to Todo CLI!
+
+--------------------
+	`);
 	prompt();
 };
 init();
