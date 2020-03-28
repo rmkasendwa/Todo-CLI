4d1e4867ccd1872bfc8988123944185e0470c3ad
Ronald M. Kasendwa <kasendwa@awamo.com>
2020-03-28 23:37:30 +0300

    Implements user prompt

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+rl.on("close", function() {
+	console.log("\nBYE BYE !!!");
+	process.exit(0);
+});
+const prompt = function() {
+	rl.question(getMenu(), function(command) {
+		switch (command.toLowerCase()) {
+			case 'q':
+				rl.close();
+				break;
+			default:
+				prompt();
+		}
+	});
+};
 const getMenu = function() {
 	return `
 Welcome to Todo CLI!
@@ -5,6 +25,10 @@ Welcome to Todo CLI!
 --------------------
 
 (v) View • ( n ) New • (cX) Complete • (dX) Delete • (q) Quit
-	`;
+
+>	`;
 };
-console.log(getMenu());
+const init = function() {
+	prompt();
+};
+init();
